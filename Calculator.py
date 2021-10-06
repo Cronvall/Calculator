42906e8caa85075962dc16843830587cb53496dc
BenjaminTWP <benjamintwpettersson@gmail.com>
2021-10-06 15:36:32 +0200

    COmmit
    massor trevliga metoder

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 # package calculator
-
+from collections import deque
 from math import nan
 from enum import Enum
 
@@ -20,14 +20,15 @@ from enum import Enum
 #
 # To run the program, run either CalculatorREPL or CalculatorGUI
 
-MISSING_OPERAND:  str = "Missing or bad operand"
-DIV_BY_ZERO:      str = "Division with 0"
+MISSING_OPERAND: str = "Missing or bad operand"
+DIV_BY_ZERO: str = "Division with 0"
 MISSING_OPERATOR: str = "Missing operator or parenthesis"
-OP_NOT_FOUND:     str = "Operator not found"
-OPERATORS:        str = "+-*/^"
+OP_NOT_FOUND: str = "Operator not found"
+OPERATORS: str = "+-*/^"
 
 class Stack():
 
+<<<<<<< Updated upstream
     def __init__(self):
         self.stack = []
 
@@ -97,21 +98,86 @@ def isOperand(ch):
     except:
         isTrue = False
     return  isTrue
+=======
+def infix_to_postfix(tokens):
+    return []  # TODO
+>>>>>>> Stashed changes
 
 
 # -----  Evaluate RPN expression -------------------
 def eval_postfix(postfix_tokens):
+<<<<<<< Updated upstream
     #TODO Calculate from RPN
     return 0
+=======
+    # TODO Calculate from RPN
+    operand1: float
+    operand2: float
+    operator: str
+
+    for i in range(len(postfix_tokens)):
+        if postfix_tokens[i] in "+-*/":
+            operator = postfix_tokens[i]
+
+            associative = get_associativity(operator)
+
+            if associative == Assoc.LEFT:
+                rpn_calculate_left(postfix_tokens, i, operator)
+
+            elif associative == Assoc.RIGHT:
+                calc_or_find: bool = find_right_associativity(postfix_tokens, i)
+                if calc_or_find:
+                    rpn_calculate_right(postfix_tokens, i, operator)
+                else:
+                    rpn_calculate_left(postfix_tokens, i, operator)
+
+    return postfix_tokens[0]
+
+
+# Function to calculate from RPN
+def remove_used_tokens(postfix_tokens, i):
+    postfix_tokens.pop(i)
+    postfix_tokens.pop(i - 1)
+
+
+def rpn_calculate_left(postfix_tokens: list, i: int, operator: str):
+    operand1 = postfix_tokens[i - 2]
+    operand2 = postfix_tokens[i - 1]
+    postfix_tokens[i - 2] = apply_operator(operator, operand1, operand2)
+
+    remove_used_tokens(postfix_tokens, i)
+
+
+def rpn_calculate_right(postfix_tokens: list, i: int, operator: str):
+    operand1 = postfix_tokens[i - 1]
+    operand2 = postfix_tokens[i + 1]
+    postfix_tokens[i - 1] = apply_operator(operator, operand1, operand2)
+
+    remove_used_tokens(postfix_tokens, i + 2)
+
+
+# Function to find
+def find_right_associativity(postfix_tokens: list, i: int):
+    if postfix_tokens[i+2] == "^":
+        return True
+    else:
+        return False
+>>>>>>> Stashed changes
 
 
 # Method used in REPL
 def eval_expr(expr: str):
     if len(expr) == 0:
         return nan
+<<<<<<< Updated upstream
     tokens = expr.split()                                       #Tokens is a list consisting of each char in the original expresion
     #postfix_tokens = infix_to_postfix(tokens)    #Returns the expresion in RTN-format
     #return eval_postfix(postfix_tokens)             #Returns the solution using the interperated RTN-expresion
+=======
+    tokens = expr.split()
+    postfix_tokens = infix_to_postfix(tokens)  # Returns the expresion in RTN-format
+    return eval_postfix(postfix_tokens)  # Returns the interperated RTN-expresion
+>>>>>>> Stashed changes
 
 
 def apply_operator(op: str, d1: float, d2: float):
@@ -154,6 +220,6 @@ def get_associativity(op: str):
 
 # ---------- Tokenize -----------------------
 def tokenize(expr: str):
-    return None   # TODO
+    return None  # TODO
 
 # TODO Possibly more methods
